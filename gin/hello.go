95a1514983ef532b67fcf0ba55c4e56d20dbb10e
Yu Watanabe <watanabe.yu+github@gmail.com>
2017-09-12 10:25:27 +0900

    create a simple web application by using gin

---
+++ gin/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!!",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
